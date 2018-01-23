0911172b75d2858dfc11e090bbea6c6dd581945c
michael.taylor <michael.taylor@my.tccd.edu>
2018-01-22 21:42:59 -0600

    Added comments

--- Classwork/HelloWorld/HelloWorld/Program.cs
+++ Classwork/HelloWorld/HelloWorld/Program.cs
@@ -6,8 +6,12 @@ using System.Threading.Tasks;
 
 namespace HelloWorld
 {
+    // Single line comment
     class Program
     {
+        /*
+         * As many lines as you want
+         */
         static void Main( string[] args )
         {
             Console.WriteLine("Hello World");
