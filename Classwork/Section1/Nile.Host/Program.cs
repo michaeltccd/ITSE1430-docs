6314bad0313ba1b09cf0e63721aab1e91ab1f24a
michael.taylor <michael.taylor@my.tccd.edu>
2018-01-24 21:43:29 -0600

    Played with primitives and local variables

---
+++ Classwork/Section1/Nile.Host/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Nile.Host
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {            
+        }
+
+        static void PlayingWithPrimitives ()
+        {
+            //Primitive
+            decimal unitPrice = 10.5M;
+
+            //Real declaration
+            //System.Decimal unitPrice2 = 10.5M;
+            Decimal unitPrice2 = 10.5M;
+
+            //Current time
+            DateTime now = DateTime.Now;
+
+            System.Collections.ArrayList items;
+        }
+
+        static void PlayingWithVariables ()
+        {
+            //Single decls
+            int hours = 0;
+            //Don't
+            //int hours;
+            //hours = 0;
+
+            double rate = 10.25;
+
+            //Still not assigned
+            //if (false)
+            //    hours = 0;
+
+            int hours2 = hours;
+
+            //Multiple decls
+            string firstName, lastName;
+
+            //string @class;
+
+            //Single assignment
+            firstName = "Bob";
+            lastName = "Miller";
+
+            //Multiple assignment
+            firstName = lastName = "Sue";
+
+            //Math ops
+            int x = 0, y = 10;
+            int add = x + y;
+            int subtract = x - y;
+            int multiply = x * y;
+            int divide = x / y;
+            int modulos = x % y;
+
+            //x = x + 10;
+            x += 10;
+            double ceiling = Math.Ceiling(rate);
+            double floor = ceiling;
+
+        }
+    }
+}
