b8dce2fa1798c75b35fe53dffed1d460199afe24
michael.taylor <michael.taylor@my.tccd.edu>
2018-08-22 18:27:41 -0500

    Hello world program

---
+++ ClassWork/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            double y = 4.65;
+            int x = 10;
+            Console.WriteLine("Hello world");
+        }
+    }
+}
