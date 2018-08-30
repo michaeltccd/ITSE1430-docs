b8dd77a52e006e34bc344fa44aab534c0c6ee00e
michael.taylor <michael.taylor@my.tccd.edu>
2018-08-29 19:31:24 -0500

    Finished string discussion. Added looping and error checking to menu.

--- ClassWork/Section1/Section1/Program.cs
+++ ClassWork/Section1/Section1/Program.cs
@@ -10,8 +10,13 @@ namespace Section1
     {
         static void Main( string[] args )
         {
-            //DisplayMenu();
-            PlayWithStrings();
+            bool notQuit;
+            do
+            {
+                notQuit = DisplayMenu();
+            } while (notQuit);
+
+            //PlayWithStrings();
         }
 
         private static void PlayWithStrings()
@@ -42,49 +47,98 @@ namespace Section1
             string name = firstName + " " + lastName;
 
             //Strings are immutable - this produces a new string
-            name = "Hello " + name;
+            //name = "Hello " + name;
+            Console.WriteLine("Hello " + name);  // Approach 1
+            Console.WriteLine("Hello {0} {1}", firstName, lastName); //Approach 2
+            string str = String.Format("Hello {0} {1}", firstName, lastName); //Approach 3
+            Console.WriteLine(str);
 
+            //Approach 4
+            Console.WriteLine($"Hello {firstName} {lastName}");
+
+            //Null vs empty
+            string missing = null;
+            string empty = "";
+            string empty2 = String.Empty;
+
+            //Checking for empty strings
+            //if (firstName.Length == 0)
+            //if (firstName != null && firstName != "")
+            if (!String.IsNullOrEmpty(firstName))
+                Console.WriteLine(firstName);
+
+            //Other stuff
+            string upperName = firstName.ToUpper();
+            string lowerName = firstName.ToLower();
+
+            //Comparison
+            bool areEqual = firstName == lastName;
+            areEqual = firstName.ToLower() == lastName.ToLower();
+            areEqual = String.Compare(firstName, lastName, true) == 0;
+
+            bool startsWithA = firstName.StartsWith("A");
+            bool endsWithA = firstName.EndsWith("A");
+            bool hasA = firstName.IndexOf("A") >= 0;
+            string subset = firstName.Substring(4);
+
+            //Clean up
+            string cleanMe = firstName.Trim(); //TrimStart, TrimEnd
+            string makeLonger = firstName.PadLeft(20); //PadRight
         }
 
-        private static void DisplayMenu()
+        private static bool DisplayMenu()
         {
-            Console.WriteLine("A)dd Movie");
-            Console.WriteLine("E)dit Movie");
-            Console.WriteLine("D)elete Movie");
-            Console.WriteLine("V)iew Movies");
-            Console.WriteLine("Q)uit");
-
-            string input = Console.ReadLine();
-            switch (input[0])
+            while (true)
             {
-                case 'A': AddMovie(); break;
-                case 'E': EditMovie(); break;
-                case 'D': DeleteMovie(); break;
-                case 'V': ViewMovies(); break;
-                case 'Q': break;
+                Console.WriteLine("A)dd Movie");
+                Console.WriteLine("E)dit Movie");
+                Console.WriteLine("D)elete Movie");
+                Console.WriteLine("V)iew Movies");
+                Console.WriteLine("Q)uit");
 
-                default: Console.WriteLine("Please enter a valid value."); break;
+                string input = Console.ReadLine();
+                switch (input[0])
+                {
+                    case 'a':
+                    case 'A': AddMovie(); return true;
+
+                    case 'e':
+                    case 'E': EditMovie(); return true;
+
+                    case 'd':
+                    case 'D': DeleteMovie(); return true;
+
+                    case 'v':
+                    case 'V': ViewMovies(); return true;
+
+                    case 'q':
+                    case 'Q': return false;
+
+                    default:
+                        Console.WriteLine("Please enter a valid value.");
+                        break;
+                };
             };
         }
 
         private static void DeleteMovie()
         {
-            throw new NotImplementedException();
+            Console.WriteLine("DeleteMovie");
         }
 
         private static void ViewMovies()
         {
-            throw new NotImplementedException();
+            Console.WriteLine("ViewMovies");
         }
 
         private static void EditMovie()
         {
-            throw new NotImplementedException();
+            Console.WriteLine("EditMovie");
         }
 
         private static void AddMovie()
         {
-            throw new NotImplementedException();
+            Console.WriteLine("AddMovie");
         }
     }
 }
