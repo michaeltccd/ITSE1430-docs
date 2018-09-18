fc5c12a138af54a6714acde7615edb92b5916aea
michael.taylor <michael.taylor@my.tccd.edu>
2018-09-17 19:17:45 -0500

    Added section 2. Created new projects for UI and business. Added start of Movie class.

---
+++ ClassWork/Section2/Itse1430.MovieLib/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Itse1430.MovieLib
+{
+    class Movie
+    {
+        public string Name;
+        //public System.String Name;
+    }
+}
