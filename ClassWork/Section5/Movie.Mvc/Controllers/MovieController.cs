1eb8969c54d5aa7d80a40e636904b073e326559f
michael.taylor <michael.taylor@my.tccd.edu>
2018-11-26 19:12:22 -0600

    Added movie controller and start of list action

---
+++ ClassWork/Section5/Movie.Mvc/Controllers/MovieController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Movie.Mvc.Controllers
+{
+    public class MovieController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+            //return View("Index");
+        }
+    }
+}
